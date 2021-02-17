c1336f8783894c8b8919019ef6e7da948b5cd046
Nains <jason20226@gmail.com>
2021-02-17 11:10:42 +0800

    2021-02-17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { ValueTransformer } from '@angular/compiler/src/util';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild("text") Text: ElementRef;
   title = 'app';
+  constructor() { }
+  ngOnInit() {
+    console.log(this.Text);
+    this.Text.nativeElement.innerHTML = this.Text.nativeElement.firstChild.textContent.replace(/\S/g, "<span>$&</span>");
+  }
+  ngAfterViewInit() {
+    anime.timeline({
+      loop: true
+    })
+      .add({
+        targets: '.text span',
+        translateY: [-600, 0],
+        scale: [10, 1],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 1500,
+        delay: anime.stagger(100)
+      })
+
+      .add({
+        targets: '.text span',
+        translateX: [0, -1000],
+        scale: [1, 1],
+        opacity: [1, 0],
+        easing: 'easeOutExpo',
+        duration: 2000,
+        delay: anime.stagger(100)
+      })
+
+      .add({
+        targets: '.text span',
+        translateX: [1000, 0],
+        scale: [1, 1],
+        opacity: [0, 1],
+        easing: 'easeOutExpo',
+        duration: 2000,
+        delay: anime.stagger(100)
+      })
+
+      .add({
+        targets: '.text span',
+        translateX: [0, 0],
+        scale: [1, 50],
+        opacity: [1, 0],
+        easing: 'easeOutExpo',
+        duration: 2000,
+        delay: anime.stagger(100)
+      })
+  }
 }
